550d21dbbecc488618e46bcd76dbd1810e954e8c
Alenchristy <94060461+Alenchristy@users.noreply.github.com>
2022-07-06 09:11:58 +0530

    Add files via upload

---
+++ ncr16.sh
@@ -0,0 +1,40 @@
+#16 shell script to find ncr of a number using function
+
+if [ $# -ne 0 ]
+then
+	echo "syntax error"
+	exit 1
+fi
+
+fact(){
+	n=$1
+	i=1
+	f=1
+	while [ $i -le $n ]
+	do
+	f=`expr $f \* $i`
+	i=`expr $i + 1`
+	done
+	return 4f
+	}
+
+echo "enter n: "
+read p
+fact $p
+a=$?
+
+echo "enter r: "
+read r
+fact $r
+b=$?
+
+
+c=`expr $p - $r`
+fact $c
+d=$?
+
+t=`expr $b \* $d`
+res=`expr $a / $t`
+
+echo "ncr of $p & $r = $res"
+ 
